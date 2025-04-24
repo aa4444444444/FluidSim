7b4d2ade1d800d5c369a5896136a14d372b613fe
Aadith C <apc3277@g.rit.edu>
2025-04-24 18:58:15 -0400

    Finished adding documentation to Particle.h

--- FluidSim/FluidSim/Particle.h
+++ FluidSim/FluidSim/Particle.h
@@ -3,17 +3,22 @@
 
 #include "../../../eigen-3.4.0/Eigen/Dense"
 
+// Class representing a single particle
 class Particle
 {
 public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+	// Constructor
 	Particle(float x, float y) {
-		m_position = Eigen::Vector2d(x, y);
-		m_velocity = Eigen::Vector2d(0.0f, 0.0f);
-		m_force = Eigen::Vector2d(0.0f, 0.0f);
-		m_rho = 0.0f;
-		m_p = 0.0f;
+		m_position = Eigen::Vector2d(x, y); // position
+		m_velocity = Eigen::Vector2d(0.0f, 0.0f); // velocity
+		m_force = Eigen::Vector2d(0.0f, 0.0f); // force
+		m_rho = 0.0f; // density
+		m_p = 0.0f; // pressure
 	}
+	
+	// Getters/Setters
 	Eigen::Vector2d getPosition() { return m_position; }
 	Eigen::Vector2d getVelocity() { return m_velocity; }
 	Eigen::Vector2d getForce() { return m_force; }
@@ -24,6 +29,7 @@ public:
 	void setForce(Eigen::Vector2d force) { m_force = force; }
 	void setRho(float rho) { m_rho = rho; }
 	void setP(float p) { m_p = p; }
+
 private:
 	Eigen::Vector2d m_position, m_velocity, m_force;
 	float m_rho, m_p;
